e01f7c41462e6f9054af34fa42d39ed02a2a5f39
Chris Ward <chris@cwardcode.com>
2015-05-08 04:47:30 +0000

    Automated table discovery and data collection

--- LoggerTester.py
+++ LoggerTester.py
@@ -1,27 +1,13 @@
 # Author: Chris Ward
 # Date: 04/20/2015
-# Version: 4/24/2015
-# Description: Reads in data from our CR850 data logger and stores to a local file
+# Version: 5/08/2015
+# Description: Reads in data from our CR850 data logger and stores to files based on table name.
+
 from datetime import datetime
-import os
-
-from pycampbellcr1000 import utils
-from pycampbellcr1000 import CR1000
-
 from InvalidDateException import InvalidDateException
-
-
-
-
-
-# ####################################x
-# _______Tables on Device ____________#
-# Tables that should be on device     #
-# TableEachScan                       #
-# Table05min                          #
-# Table15min                          #
-# Table24hr                           #
-# ####################################x
+from pycampbellcr1000 import CR1000
+from pycampbellcr1000 import utils
+import os
 
 # Holds the device's mapped location
 location = "/dev/ttyO0"
@@ -31,24 +17,14 @@ port = "115200"
 print('connecting to logger...')
 
 # The device we're connecting to,
-device = CR1000.from_url('serial:/' + location + ":" + port)
+#device = CR1000.from_url('serial:/' + location + ":" + port)
 print('connected to ' + location + ':' + port)
 
-# ###############################################################
-# TODO: Remove both calls below, just a simple connection test. $
-# ###############################################################
-
-# Return all files on device
-files = device.list_files()
-# If any files exist, print
-print('Files on device:')
-print(files)
 # Return all tables from device
 tables = device.list_tables()
 # Print out all tables on device
-print('Tables on device:')
+print('Tables on device: ')
 print(tables)
-
 
 # Date to begin data collection
 start_date = ""
@@ -67,11 +43,11 @@ end_date_form = ""
 # TODO: Later, automate for specified interval if needed
 while True:
     try:
-        start_date = input('Enter a start date to collect data: ')
-        end_date = input('Enter an end date to collect data: ')
+        start_date = raw_input('Enter a start date to collect data: ')
+        end_date = raw_input('Enter an end date to collect data: ')
         print('Formatting date...')
-        start_date_form = datetime.strptime(str(start_date), "%b %d %Y")
-        end_date_form = datetime.strptime(str(end_date), "%b %d %Y")
+        start_date_form = datetime.strptime(start_date, "%b %d %Y")
+        end_date_form = datetime.strptime(end_date, "%b %d %Y")
         # Ensure start date is before end date
         if start_date_form > end_date_form:
             raise InvalidDateException('Invalid date!')
@@ -87,7 +63,7 @@ while True:
 """
 
 
-def scantable(table_name):
+def collectdata(table_name):
     table_file = os.open(table_name + '.csv', os.O_WRONLY | os.O_APPEND | os.O_CREAT)
     table = device.get_data(table_name, start_date_form, end_date_form)
     table_csv = utils.dict_to_csv(table, ",", header=True)
@@ -95,11 +71,13 @@ def scantable(table_name):
     os.close(table_file)
 
 """
-" Call tables to scan.
+" Iterate through tables list, and collect data from each
 """
-# TODO: Make this automatic from get_tables()
 
-scantable('TableEachScan')
-scantable('Table05Min')
-scantable('Table15Min')
-scantable('Table24Hr')
+for table in tables:
+    collectdata(table)
+
+#collectdata('TableEachScan')
+#collectdata('Table05Min')
+#collectdata('Table15Min')
+#collectdata('Table24Hr')
