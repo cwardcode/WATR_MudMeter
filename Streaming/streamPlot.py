c5cb93515211bc15c86c2bceb49789421cf61c02
Chris Ward <chris@cwardcode.com>
2015-05-22 02:37:13 -0400

    Successfully streams data from updating files. Added test data.

--- Streaming/streamPlot.py
+++ Streaming/streamPlot.py
@@ -1,6 +1,5 @@
-from plotly.graph_objs import *
+from plotly.graph_objs import Stream, Scatter, Layout, Data, Figure, XAxis,YAxis
 import csv
-import datetime
 import plotly.plotly as py
 import plotly.tools as tls
 import numpy as np
@@ -8,30 +7,30 @@ import time
 
 stream_ids = tls.get_credentials_file()['stream_ids']
 
-stream_id=stream_ids[0]
+stream_id = stream_ids[0]
 
-stream = Stream(
-        token=stream_id,
-        maxpoints=80
+stream_obj = Stream(
+    token=stream_id,
+    maxpoints=80
 )
 
 trace1 = Scatter(
-        x=[],
-        y=[],
-        mode='lines+markers',
-        stream=stream
+    x=[],
+    y=[],
+    mode='lines+markers',
+    stream=stream_obj
 )
 
 data = Data([trace1])
 
 layout = Layout(
-        title='NTU Over Time',
-        xaxis=XAxis(
-            title='Date Time'
-        ),
-        yaxis=YAxis(
-            title='Turbidity(NTU)'
-        )
+    title='NTU Over Time',
+    xaxis=XAxis(
+        title='Date Time'
+    ),
+    yaxis=YAxis(
+        title='Turbidity(NTU)'
+    )
 )
 
 fig = Figure(data=data, layout=layout)
@@ -40,40 +39,49 @@ unique_url = py.plot(fig, filename='NTUDataStream')
 
 stream_link = py.Stream(stream_id)
 
-def get_csv_data(filepath, row_id):
-    data = []
+lastLine = 0
+
+
+def get_csv_data(filepath, row_id, row_num):
+    data_array = []
     with open(filepath, 'r') as data_file:
         reader = csv.reader(data_file)
         hrow = reader.next()
-        colID = hrow.index(row_id)
+        colid = hrow.index(row_id)
+        while reader.line_num != (row_num + 1):
+                reader.next()
         for row in reader:
-            data.append(row[colID])
-        npArray = np.array(data)
-        return npArray
+            data_array.append(row[colid])
+        nparray = np.array(data_array)
+        return nparray
 
-NTU = get_csv_data('./data/TableEachScan.csv','TurbNTU')
-Date = get_csv_data('./data/TableEachScan.csv','Datetime')
+NTU = get_csv_data('./data/TableEachScan.csv', 'TurbNTU', 0)
+Date = get_csv_data('./data/TableEachScan.csv', 'Datetime', 0)
+
+lastLine = Date.size
+print("Last line read was: " + str(lastLine))
 
 stream_link.open()
 
 i = 0
-k = 5
 N = Date.size
 
 time.sleep(5)
 
-while i<N:
+while i < N:
     x = Date[i]
     y = NTU[i] 
     stream_link.write(dict(x=x, y=y))
     i += 1
     if i == N:
-	i=0
-	NTU = get_csv_data('./data/TableEachScan.csv','TurbNTU')
-	Date = get_csv_data('./data/TableEachScan.csv','Datetime')
-      	print("New data loaded: " + str(NTU)) 
-    #time.sleep(0.08)
-    time.sleep(1.00)
+        i = 0
+        NTU = []
+        Date = []
+        NTU = get_csv_data('./data/TableEachScan.csv', 'TurbNTU', lastLine + 1)
+        Date = get_csv_data('./data/TableEachScan.csv', 'Datetime', lastLine + 1)
+        lastLine = lastLine + Date.size
+        print("New data EOL is: " + str(lastLine))
+        N = Date.size
+    time.sleep(0.50)
 
 stream_link.close()
-
